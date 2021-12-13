53ca4f7bdaddaeb26948aabc6169aab0467b3da2
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-12-13 23:31:32 +0900

    Create OrderListVO.java

---
+++ ReviewService/src/main/java/restaurant/orderManagement/service/OrderListVO.java
@@ -0,0 +1,26 @@
+package restaurant.orderManagement.service;
+
+public class OrderListVO {
+	int order_index;
+	int menu_index;
+	int count;
+	public int getOrder_index() {
+		return order_index;
+	}
+	public void setOrder_index(int order_index) {
+		this.order_index = order_index;
+	}
+	public int getMenu_index() {
+		return menu_index;
+	}
+	public void setMenu_index(int menu_index) {
+		this.menu_index = menu_index;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+}
