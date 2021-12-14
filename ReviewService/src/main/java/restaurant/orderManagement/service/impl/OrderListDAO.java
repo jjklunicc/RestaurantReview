4e2e69ddd331c3bf1ac7a09aea5c513356db8697
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-12-14 17:24:04 +0900

    Update OrderListDAO.java

--- ReviewService/src/main/java/restaurant/orderManagement/service/impl/OrderListDAO.java
+++ ReviewService/src/main/java/restaurant/orderManagement/service/impl/OrderListDAO.java
@@ -2,13 +2,7 @@ package restaurant.orderManagement.service.impl;
 
 import org.springframework.stereotype.Repository;
 
-import restaurant.orderManagement.service.OrderListVO;
-import egovframework.rte.psl.dataaccess.EgovAbstractDAO;
+@Repository("orderListDAO")
+public class orderListDAO {
 
-@Repository("OrderListDAO")
-public class OrderListDAO extends EgovAbstractDAO{
-	public String insertOrderList(OrderListVO vo) throws Exception
-	{
-		return (String)insert("orderListDAO.insertOrderList", vo);		
-	}
 }
