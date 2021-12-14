0e032949f30ddc0c54859b893f1d4ac2d3720ab2
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-12-14 18:05:50 +0900

    commit

---
+++ ReviewService/src/main/java/restaurant/orderManagement/service/ShoppingBagVO.java
@@ -0,0 +1,26 @@
+package restaurant.orderManagement.service;
+
+public class ShoppingBagVO {
+	int user_index;
+	int menu_index;
+	int count;
+	public int getUser_index() {
+		return user_index;
+	}
+	public void setUser_index(int user_index) {
+		this.user_index = user_index;
+	}
+	public int getMenu_index() {
+		return menu_index;
+	}
+	public void setMenu_index(int menu_index) {
+		this.menu_index = menu_index;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+}
